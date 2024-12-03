e640fe1ed4cfbc03e7b4fd1a3022aa73d333e733
LILIANA CAZORLA <angelsuarez131@gmail.com>
2024-12-02 23:35:31 -0500

    Primer commit cl2

---
+++ src/main/java/pe/edu/_0/DAWI_SUAREZ_CAZORLA_ANGELRODRIGO/DawiSuarezCazorlaAngelrodrigoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu._0.DAWI_SUAREZ_CAZORLA_ANGELRODRIGO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiSuarezCazorlaAngelrodrigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiSuarezCazorlaAngelrodrigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/_0/DAWI_SUAREZ_CAZORLA_ANGELRODRIGO/DawiSuarezCazorlaAngelrodrigoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu._0.DAWI_SUAREZ_CAZORLA_ANGELRODRIGO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiSuarezCazorlaAngelrodrigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
